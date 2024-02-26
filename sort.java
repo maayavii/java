f59058ddfccabbe0692be0733a504a3e35b024f7
AMAL MANOJ <145776188+maayavii@users.noreply.github.com>
2024-02-26 12:42:52 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Sort{
+public static void main(String a[]){
+String str1="hello";
+
+char[] cArray=str1.toCharArray();
+Arrays.sort(cArray);
+String sortedstr=new String(cArray);
+System.out.println(sortedstr);
+System.out.println("amal manoj -12");
+System.out.println("26,02,2024");}}
